ed199637d6695f0e9403e844ebdf8860cb4cbf81
loica <loica.etudiant@gmail.com>
2022-08-04 00:17:16 +0300

    first

--- src/main/java/BlackAndWhiteTest.java
+++ src/main/java/BlackAndWhiteTest.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class BlackAndWhiteTest {
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class BlackAndWhiteTest {
+
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:/Users/loica/Downloads/téléchargement.jpg");
+        BufferedImage orginalImage = ImageIO.read(file);
+
+        BufferedImage blackAndWhiteImg = new BufferedImage(
+                orginalImage.getWidth(), orginalImage.getHeight(),
+                BufferedImage.TYPE_BYTE_BINARY);
+
+        Graphics2D graphics = blackAndWhiteImg.createGraphics();
+        graphics.drawImage(orginalImage, 0, 0, null);
+
+        ImageIO.write(blackAndWhiteImg, "png", new File("C:/Users/loica/Downloads/téléchargementBW.jpg"));
+    }
+
 }
