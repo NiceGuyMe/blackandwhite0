684ef8d8a3c7e37e8f22911d8aebb117d5c96d80
loica <loica.etudiant@gmail.com>
2022-08-03 23:49:52 +0300

    Initial commit

---
+++ src/main/java/com/example/blackandwhite0/Blackandwhite0Application.java
@@ -0,0 +1,13 @@
+package com.example.blackandwhite0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blackandwhite0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blackandwhite0Application.class, args);
+	}
+
+}
